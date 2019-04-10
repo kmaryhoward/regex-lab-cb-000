d87ad3f69f39cffc12f66de9977646a2899852d6
Kathleen Howard <kmaryhoward@gmail.com>
2019-04-10 13:56:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,42 @@
 def starts_with_a_vowel?(word)
-
+  a = word.match((/^[aeiou]/i))
+  a.nil? ? false : true
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(string)
+  a = []
+  string.split.each do |item|
+    if item.match(/^un|ing/)
+      a << item
+    end
+  end
+  a
 end
+
 
 def words_five_letters_long(text)
-
+  words = []
+  text.split.each do |item|
+    if item.match(/^[a-zA-Z]{5}$/)
+    words << item
+    end
+   end
+  words
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  compare = text.split
+  words = compare.grep(/^[A-Z]/)
 
+  if words[0] == compare[0] && compare[-1].match(/[[:punct:]]$/)
+    true
+      else
+    false
+
+
+
+  end
 end
 
 def valid_phone_number?(phone)
